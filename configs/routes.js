59f1f50e925704476996172c828798f730567699
Vasilii Diakov <vasilij.dyakov@gmail.com>
2015-10-25 16:38:31 +0300

    Правки для прохождения eslint

--- configs/routes.js
+++ configs/routes.js
@@ -1,5 +1,3 @@
-import _ from 'lodash';
-
 export default {
     '/': 'index/IndexPage.jsx'
 };
--- pages/Error404Page.jsx
+++ pages/Error404Page.jsx
@@ -1,6 +1,7 @@
 import React from 'react';
+import PageComponent from '../common/PageComponent.jsx';
 
-export default class Error404Page extends React.Component {
+export default class Error404Page extends PageComponent {
     render() {
         return <div>404 Error</div>;
     }
