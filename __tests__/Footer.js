9da611095ee1e4345957b5385c593f438c6f05ee
Vasilii Diakov <vasilij.dyakov@gmail.com>
2015-10-31 01:15:53 +0300

    Настройка jest

---
+++ __tests__/Footer.js
@@ -0,0 +1,34 @@
+jest.dontMock('../components/footer/Footer.jsx');
+jest.dontMock('../common/ContextProvider.jsx');
+
+import React from 'react';
+import ReactDOM from 'react-dom';
+import TestUtils from 'react-addons-test-utils';
+
+const Footer = require('../components/footer/Footer.jsx'),
+    ContextProvider = require('../common/ContextProvider.jsx');
+
+var contextMock = {
+    getStore() {
+        return {
+            getState() {
+                return {};
+            }
+        };
+    }
+};
+
+describe('Footer', () => {
+
+    it('should', () => {
+
+        // Render a checkbox with label in the document
+        var checkbox = TestUtils.renderIntoDocument(<ContextProvider context={contextMock}>
+                {() => <Footer/>}
+            </ContextProvider>),
+            footerNode = ReactDOM.findDOMNode(checkbox);
+
+        expect(footerNode.textContent).toEqual('Footer text');
+    });
+
+});
