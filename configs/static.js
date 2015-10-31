d0bb27ded197849a69e7bc4254944ea79007b1c8
Vasilii Diakov <vasilij.dyakov@gmail.com>
2015-10-31 16:30:07 +0300

    Начало браузерной интеграции

--- configs/static.js
+++ configs/static.js
@@ -1,6 +1,9 @@
 export default {
     enabledPatterns: [
         '^/node_modules/less/dist/less\.js$',
+        '^/node_modules/babel-core/browser\.js$',
+        '^/(actions|client|configs|stores)/.*\.js$',
+        '^/(common|components|pages)/.*\.(js|jsx)$',
         '^/components/.*\.(less|png|svg|jpg)$'
     ].map(pattern => new RegExp(pattern))
 };
