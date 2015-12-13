4d29e985b27ce8b20491eca90f5c47075241a142
Vasilii Diakov <vasilij.dyakov@gmail.com>
2015-12-13 10:51:42 +0300

    fix test

--- __tests__/Footer.js
+++ __tests__/Footer.js
@@ -1,32 +1,17 @@
 jest.dontMock('../components/footer/Footer.jsx');
-jest.dontMock('../common/ContextProvider.jsx');
 
 import React from 'react';
 import ReactDOM from 'react-dom';
 import TestUtils from 'react-addons-test-utils';
 
-const Footer = require('../components/footer/Footer.jsx'),
-    ContextProvider = require('../common/ContextProvider.jsx');
-
-var contextMock = {
-    getStore() {
-        return {
-            getState() {
-                return {};
-            }
-        };
-    }
-};
+const Footer = require('../components/footer/Footer.jsx');
 
 describe('Footer', () => {
 
     it('should have correct text inside', () => {
 
-        // Render a checkbox with label in the document
-        var checkbox = TestUtils.renderIntoDocument(<ContextProvider context={contextMock}>
-                {() => <Footer/>}
-            </ContextProvider>),
-            footerNode = ReactDOM.findDOMNode(checkbox);
+        var footer = TestUtils.renderIntoDocument(<Footer/>),
+            footerNode = ReactDOM.findDOMNode(footer);
 
         expect(footerNode.textContent).toEqual('Footer text');
     });
